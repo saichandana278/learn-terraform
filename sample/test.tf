bfe5f0bc10a369ee679f2a6b7bcd0d4710865a60
chandana <chandanatadimarri278@gmail.com>
2023-02-25 20:15:21 +0530

    terraform test

--- sample/test.tf
+++ sample/test.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-058fed8fc4f587f9e"]
-  tags                   = {
-    name = "test"
+  tags = {
+    Name = "test"
   }
 }
